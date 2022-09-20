4d95401fcf90fd09bc01f34617a0faa3da600cfc
HarryMeng <harry.mld.2010@gmail.com>
2022-09-19 20:39:26 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};// fix me {1:1, 2:2,etc }
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // If item is not in the object, then add it
+  if (this._storage[item] === undefined) {
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  return this._storage[item] !== undefined;
 };
 
 setPrototype.remove = function(item) {
+  if (this._storage[item] !== undefined) {
+    delete this._storage[item];
+  }
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: What is the time complexity of the above functions? O(1)
  */
